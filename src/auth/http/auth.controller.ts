b7e9e6ba3a9d6e4d0ad91c992e8735f6a0ea8be1
Ale Sanchez <alesanchezmedina@gmail.com>
2025-11-24 09:12:50 +0100

    fix(auth): :bug: Change default code for login to a more idiomatic one

--- src/auth/http/auth.controller.ts
+++ src/auth/http/auth.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  HttpCode,
   HttpStatus,
   Post,
   Res,
@@ -32,6 +33,7 @@ export class AuthController {
   @PublicEndpoint()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
+  @HttpCode(HttpStatus.OK)
   login(@CurrentUser() user: AuthUser) {
     const jwt = this.signJwtUsecase.execute(user);
 
